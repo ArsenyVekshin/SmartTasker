34db5468dfae7a2fa1855a93e1aa2fa4e189007a
Deyzer2566 <a.kozodoy@yandex.ru>
2024-11-09 12:34:45 +0300

    durations > 0

--- constraints.sql
+++ constraints.sql
@@ -3,3 +3,12 @@ ADD CONSTRAINT unique_email UNIQUE (email);
 
 ALTER TABLE MeetingMembers
 ADD CONSTRAINT unique_meeting_members UNIQUE (meet_id, task_id, user_id);
+
+ALTER TABLE Task
+ADD CONSTRAINT positive_duration CHECK (duration > 0);
+
+ALTER TABLE TimeInterval
+ADD CONSTRAINT duration_positive CHECK (duration > 0);
+
+ALTER TABLE Meeting
+ADD CONSTRAINT duration_positive CHECK (duration > 0);
